64776df98d7d94cf143d6c6b328e430b68639fb2
Shada haddad <Shada11haddad11@gmail.com>
2025-09-29 23:40:23 +0300

    solved inshallah

--- jest.config.js
+++ jest.config.js
@@ -7,13 +7,18 @@ module.exports = {
     {
       displayName: "server",
       testEnvironment: "node",
-      testMatch: ["<rootDir>/tests/**/?(*.)+(server|integration).test.js"]
+      testMatch: [
+        "<rootDir>/tests/server.test.js",
+        "<rootDir>/tests/integration.test.js"
+      ]
     },
     {
       displayName: "client",
       testEnvironment: "jsdom",
       setupFiles: ["<rootDir>/tests/setup.js"],
-      testMatch: ["<rootDir>/tests/**/?(*.)+client.test.js"]
+      testMatch: [
+        "<rootDir>/tests/client.test.js"
+      ]
     }
   ]
 };
